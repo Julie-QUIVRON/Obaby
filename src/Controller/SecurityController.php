f9eeeea2bb2d86d5b46ab36f71e4e15965779fe0
LucasMachut <machut.lucas@gmail.com>
2022-07-29 14:41:16 +0200

    initialize function delete

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 
 class SecurityController extends AbstractController
 {
@@ -33,4 +42,13 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    /**
+     * @Route("/user/{id}/delete", name="user_delete")
+     */
+    public function deleteUser($id, UserRepository $userRepository)
+    {
+      
+    }
+
 }
